70d3dea5abb3be9838395d89e28d028b2a34cc1d
Tornike <chargeishvilitornike@gmail.com>
2025-07-04 15:45:03 +0400

    replace <p> with <span> and add input field for amount entry

--- src/Components/Leftside/Leftside.jsx
+++ src/Components/Leftside/Leftside.jsx
@@ -1,3 +1,4 @@
+import { FaPoundSign } from "react-icons/fa";
 import calculator from "../../assets/images/icon-calculator.svg";
 import "./Leftside.scss";
 
@@ -7,10 +8,29 @@ function Leftside() {
             <div className="calculator-wrapper">
                 <div className="calculator-header">
                     <h1 className="calculator-title">Mortgage Calculator</h1>
-                    <button>
-                        <p className="clear-button">Clear All</p>
+                    <button className="clear-button">
+                        <span>Clear All</span>
                     </button>
                 </div>
+
+                <form>
+                    <div className="mortgageAmount-container">
+                        <label htmlFor="mortgageAmount-input">
+                            Mortgage Amount
+                        </label>
+
+                        <div className="mortgageAmount-input-wrapper">
+                            <div className="input-icon-wrapper">
+                                <FaPoundSign className="input-icon" />
+                            </div>
+                            <input
+                                id="mortgageAmount-input"
+                                type="number"
+                                placeholder="Enter amount"
+                            />
+                        </div>
+                    </div>
+                </form>
             </div>
         </div>
     );
