b426f0ac3fce450177ef9e48a6c99c357c0f019a
Tornike <chargeishvilitornike@gmail.com>
2025-07-17 16:22:55 +0400

    add perent to child focus functionality

--- src/Components/Leftside/MortgageType.jsx
+++ src/Components/Leftside/MortgageType.jsx
@@ -1,14 +1,9 @@
 function MortgageType({ label_text, name }) {
     return (
-        <div className="mortgage-type__option" tabIndex="0">
-            <input
-                type="radio"
-                id={label_text}
-                name={name}
-                value={label_text}
-            />
-            <label htmlFor={label_text}>{label_text}</label>
-        </div>
+        <label className="mortgage-type__option">
+            <input type="radio" name={name} value={label_text} />
+            <span>{label_text}</span>
+        </label>
     );
 }
 
