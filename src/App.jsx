bafe44d3579a2c132ae7a9eba857ca1d3ebf3ba7
Tornike <chargeishvilitornike@gmail.com>
2025-08-02 11:29:11 +0400

    add comments and another useEffect for handel width resize

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,10 @@ import "./App.scss";
 import Leftside from "./Components/Leftside/Leftside";
 import Rightside from "./Components/Rightside/Rightside";
 import { useState, useEffect } from "react";
+import "./Mobile.scss";
 
 function App() {
+    const [width, setWidth] = useState(window.innerWidth);
     const [containerHeight, setContainerHeight] = useState({
         height: "min(95%, 450px)",
     });
@@ -16,18 +18,38 @@ function App() {
     });
 
     useEffect(() => {
+        const handleResize = () => setWidth(window.innerWidth);
+        window.addEventListener("resize", handleResize);
+
+        return () => window.removeEventListener("resize", handleResize);
+    }, []);
+
+    useEffect(() => {
         let false_value_counter = 0;
 
         for (let each in isInvalid) {
-            if (isInvalid[each] === false) {
+            if (!isInvalid[each]) {
                 false_value_counter++;
             }
         }
+        /*
+            We adjust the height of the <main> tag based on how many input fields are invalid.
+            For each invalid input, we add 25px to make room for error messages.
 
+            If the screen width is greater than 600px:
+                - Start height is 450px.
+            If the screen width is 600px or less:
+                - Start height is 520px to better accommodate the vertical layout on smaller screens.
+
+            This helps ensure the layout displays correctly without elements overlapping or being cut off.
+        */
         setContainerHeight({
-            height: `min(95%, ${450 + false_value_counter * 25}px)`,
+            height:
+                width > 600
+                    ? `min(95%, ${450 + false_value_counter * 25}px)`
+                    : `min(95%, ${520 + false_value_counter * 25}px)`,
         });
-    }, [isInvalid]);
+    }, [isInvalid, width]);
 
     return (
         <div className="container">
@@ -37,7 +59,7 @@ function App() {
                     setIsInvalid={setIsInvalid}
                     setContainerHeight={setContainerHeight}
                 />
-                <Rightside />
+                <Rightside isInvalid={isInvalid} />
             </main>
         </div>
     );
